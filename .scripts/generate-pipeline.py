04957d0aa84c577d7a3ab27d67a9b062fafb33d3
Alex Thomae <git@alex.thom.ae>
2020-09-08 20:30:13 +0200

    Add support for multi-arch build
    * Add support for `-p` in the generate-pipeline.py script
    * Add the variable `TARGET_PLATFORM` when generating the child pipelines
    * Remove the `--load` param for `build`
    
    Signed-off-by: Alex Thomae <git@alex.thom.ae>

--- .scripts/generate-pipeline.py
+++ .scripts/generate-pipeline.py
@@ -9,21 +9,33 @@ import argparse
 import json
 from jinja2 import Template
 
-parser = argparse.ArgumentParser(description='Generate pipeline file for docker-qemu')
+parser = argparse.ArgumentParser(description='Generate child pipelines file based on a jinja2 template')
 
+parser.add_argument(
+    '-p',
+    '--target-platform',
+    default="linux/amd64",
+    dest="target_platform",
+    help=(
+        'comma separated list of target platforms: linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 '
+        '(default: linux/amd64)'
+    ),
+)
 parser.add_argument(
     '-t',
     '--template-file',
     default=".scripts/pipeline.yml.j2",
     type=argparse.FileType('r'),
-    dest="template_file"
+    dest="template_file",
+    help='(default: .scripts/pipeline.yml.j2)',
 )
 parser.add_argument(
     '-v',
     '--versions-file',
     default="versions.json",
     type=argparse.FileType('r'),
-    dest="versions_file"
+    dest="versions_file",
+    help='(default: versions.json)',
 )
 
 args = parser.parse_args()
@@ -43,6 +55,6 @@ for version in versions:
 
 versions[latest]['latest'] = True
 
-template = Template(args.template_file.read()).render(versions=versions)
+template = Template(args.template_file.read()).render(versions=versions, target_platform=args.target_platform)
 
 print(template)
